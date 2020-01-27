6c7087be1a3c1ea096a1b23cbe434ba87ac6a24c
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-27 09:18:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
 
   if array.length < 3
     return array.join(" and ")
- else array.length === 3
+  else array.length === 3
    return array[0..-2].join(', ') + ", and " + array[-1] if array.length > 1
  end
  
