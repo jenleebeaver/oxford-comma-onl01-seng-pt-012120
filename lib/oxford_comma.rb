9bdf9d1a693f34fe2cc4b062295f6c8d510acbd7
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-27 09:01:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 
 def oxford_comma(array)  
  if array.length === 3 {
-   return array[0..-2].join(', ') + ", and " + array[-1] if array.length > 1   
- } 
+   return array[0..-2].join(', ') + ", and " + array[-1] if array.length > 1
+ }
    return array.join(" and ")
 end
