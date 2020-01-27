43e3c8dc55adb4a5f385332e5dbd26add792ad4d
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-27 09:11:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,13 @@
 
 def oxford_comma(array)
  if array.length === 3
-   return arr.slice(0, -1).join(', ') + ', and ' + arr.slice(-1)
+   return array[0..-2].join(', ') + ", and " + array[-1] if array.length > 1
  end
- #if array.length > 3
-   #return  return array[0..-2].join(', ') + ", and " + array[-1] if array.length > 1
- #end
+
+ if array.length > 3
+   return array[0..-2].join(', ') + ", and " + array[-1] if array.length > 1
+ end
+ 
+ end
    return array.join(" and ")
 end
